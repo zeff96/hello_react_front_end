bd3df94f31fa4737b27210485c49ed8ee4c7f233
zeff96 <zeffo1975@gmail.com>
2023-07-11 09:26:38 +0300

    Add file path for Greetings component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Greetings from './components/greetings';
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route exact path="/" element={<Greetings />}/>
+        <Route exact path="/" element={<Greetings />} />
       </Routes>
     </Router>
   );

aa1ee419a2a1ff507293d4615c7931e848c4d3b6
zeff96 <zeffo1975@gmail.com>
2023-07-11 07:46:44 +0300

    Add store to app entry point

--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
+import setupStore from './redux/store';
 import App from './App';
+
+const store = setupStore()
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
